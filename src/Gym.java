b6556c9bc2e327ce54c2a167d7918565330c762a
dendub <ddubovskyy@gmail.com>
2019-06-16 16:43:49 +0200

    wq

--- src/Gym.java
+++ src/Gym.java
@@ -1,71 +1,66 @@
-public class Gym
-{
-    private String name;
-    private String cityAdress;
-    private int numberOfSections;
-    private String sectionName1;
-    private String sectionName2;
-    private int numberOfInventory;
-
-    public Gym(String name_, String cityAdress_, int numberOfSections_, String sectionName1_,String sectionName2_, int numberOfInventory_) {
-        name = name_;
-        cityAdress = cityAdress_;
-        numberOfSections = numberOfSections_;
-        sectionName1 = sectionName1_;
-        sectionName2 = sectionName2_;
-        numberOfInventory = numberOfInventory_;
-    }
-
-    public void addInventory(int a) {
-        numberOfInventory = a + numberOfInventory;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCityAdress() {
-        return cityAdress;
-    }
-
-    public void setCityAdress(String cityAdress) {
-        this.cityAdress = cityAdress;
-    }
-
-    public int getNumberOfSections() {
-        return numberOfSections;
-    }
-
-    public void setNumberOfSections(int numberOfSections) {
-        this.numberOfSections = numberOfSections;
-    }
-
-    public String getSectionName1() {
-        return sectionName1;
-    }
-
-    public void setSectionName1(String sectionName1) {
-        this.sectionName1 = sectionName1;
-    }
-
-    public String getSectionName2() {
-        return sectionName2;
-    }
-
-    public void setSectionName2(String sectionName2) {
-        this.sectionName2 = sectionName2;
-    }
-
-    public int getNumberOfInventory() {
-        return numberOfInventory;
-    }
-
-    public void setNumberOfInventory(int numberOfInventory) {
-        this.numberOfInventory = numberOfInventory;
-    }
-}
+//public class Gym extends GymNetwork {
+//    private String cityAdress;
+//    private String sectionName1;
+//    private String sectionName2;
+//    private int numberOfInventory;
+//    private static int numberOfGyms=0;
+//
+//    public Gym(String gymName1_,String gymName2_, String cityAdress_, String sectionName1_,String sectionName2_, int numberOfInventory_) {
+//        super(gymName1_,gymName2_);
+//        cityAdress = cityAdress_;
+//        sectionName1 = sectionName1_;
+//        sectionName2 = sectionName2_;
+//        numberOfInventory = numberOfInventory_;
+//        numberOfGyms++;
+//    }
+//
+//    public void addInventory(int a) {
+//        if (a == 1) {
+//            numberOfInventory++;
+//        } else {
+//            numberOfInventory = a + numberOfInventory;
+//        }
+//    }
+//
+//    public int getNumberOfGyms() {
+//        return numberOfGyms;
+//    }
+//
+//    public void setNumberOfGyms(int numberOfGyms) {
+//        Gym.numberOfGyms = numberOfGyms;
+//    }
+//
+//
+//    public String getCityAdress() {
+//        return cityAdress;
+//    }
+//
+//    public void setCityAdress(String cityAdress) {
+//        this.cityAdress = cityAdress;
+//    }
+//
+//
+//    public String getSectionName1() {
+//        return sectionName1;
+//    }
+//
+//    public void setSectionName1(String sectionName1) {
+//        this.sectionName1 = sectionName1;
+//    }
+//
+//    public String getSectionName2() {
+//        return sectionName2;
+//    }
+//
+//    public void setSectionName2(String sectionName2) {
+//        this.sectionName2 = sectionName2;
+//    }
+//
+//    public int getNumberOfInventory() {
+//        return numberOfInventory;
+//    }
+//
+//    public void setNumberOfInventory(int numberOfInventory) {
+//        this.numberOfInventory = numberOfInventory;
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,106 @@
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
+
+        //SCANNER
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter country of gymNetwork:" );
+        String country=in.nextLine();
+        System.out.println("Number of gyms: " );
+        int numberOfGyms=Integer.parseInt(in.nextLine());
+        System.out.println("Name of gym1: " );
+        String gymName1=in.nextLine();
+        System.out.println("Name of gym2: " );
+        String gymName2=in.nextLine();
+        System.out.println("Month income: " );
+        int monthIncome=Integer.parseInt(in.nextLine());
+        //        System.out.println("Name of the gym");
+//        String name = in.nextLine();
+
+        System.out.println("City: ");
+        String cityAdress = in.nextLine();
+        System.out.println("Name of the section1: ");
+        String sectionName1 = in.nextLine();
+        System.out.println("Name of the section2: ");
+        String sectionName2 = in.nextLine();
+        System.out.println("Number of inventory: ");
+        int numberOfInventory = Integer.parseInt(in.nextLine());
+
+
+        System.out.println("City gym1: ");
+        String cityAdress1 = in.nextLine();
+        System.out.println("Name of the section1 gym1: ");
+        String sectionName11 = in.nextLine();
+        System.out.println("Name of the section2 gym1: ");
+        String sectionName12 = in.nextLine();
+        System.out.println("Number of inventory gym1: ");
+        int numberOfInventory1 = Integer.parseInt(in.nextLine());
+        //GYM NETWORK DECLARATION
+        GymNetwork net = new GymNetwork(country,numberOfGyms,gymName1,gymName2,monthIncome);
+
+
+        //FIRST GYM
+        Gym1 first = new Gym1(gymName1,gymName2,country, cityAdress,  sectionName1, sectionName2, numberOfInventory);
+        System.out.println("Name of the gym: ");
+        System.out.println(first.getGymName1());
+        System.out.println("Country of the gym: ");
+        System.out.println(first.getCountry());
+        System.out.println("City where gym is placed: ");
+        System.out.println(first.getCityAdress());
+        System.out.println("Name of the section1: ");
+        System.out.println(first.getSectionName1());
+        System.out.println("Name of the section2: ");
+        System.out.println(first.getSectionName2());
+        System.out.println("Number of inventory: ");
+        System.out.println(first.getNumberOfInventory());
+        System.out.println("Number of gyms: ");
+        System.out.println(first.getNumberOfGyms());
+
+        //SECOND GYM
+        Gym2 second = new Gym2(gymName1,gymName2,country, cityAdress1,sectionName11,sectionName12,numberOfInventory1);
+        System.out.println("Name of the gym: ");
+        System.out.println(second.getGymName2());
+        System.out.println("Country of the gym: ");
+        System.out.println(second.getCountry());
+        System.out.println("City where gym is placed: ");
+        System.out.println(second.getCityAdress1());
+        System.out.println("Name of the section1: ");
+        System.out.println(second.getSectionName11());
+        System.out.println("Name of the section2: ");
+        System.out.println(second.getSectionName12());
+        System.out.println("Number of inventory: ");
+        System.out.println(second.getNumberOfInventory1());
+        System.out.println("Number of gyms: ");
+        System.out.println(second.getNumberOfGyms());
+
+
+
+    }
+}
+
+
+
+
+//    Gym first = new Gym("CityFit", "Poznan",2, "Cardio", "Strength", 1232);
+//        System.out.println(first.getNumberOfInventory());
+//                first.addInventory(2);
+//                System.out.println(first.getNumberOfInventory());
+//                first.addInventory(1);
+//                System.out.println(first.getNumberOfInventory());
+//                System.out.println(first.getNumberOfSections());
+//                System.out.println(first.getNumberOfGyms());
+
+
+
+
+
+
+
+//HUMAN
+
 //        Human first = new Human("Denys", "Dubovskyy", 19);
 //        System.out.println("Hello World!");
 //        System.out.println(first.getAge());
@@ -11,13 +111,8 @@ public class Main {
 
 
 
-//        Gym first = new Gym("CityFit", "Poznan", 2, "Cardio", "Strength", 1232);
-//        System.out.println(first.getNumberOfInventory());
-//        first.addInventory(2);
-//        System.out.println(first.getNumberOfInventory());
 
-
-
+//STACK AND QUEUE
 
 //        Stack test = new Stack();
 //        test.pop();
@@ -29,15 +124,9 @@ public class Main {
 //        test.pop();
 //        System.out.println(test.pop());
 
-        Queue test = new Queue();
-        test.enqueu(0);
-        test.enqueu(4);
-        test.enqueu(21);
-
-        test.dequeue();
-
-
-
-
-    }
-}
+//        Queue test = new Queue();
+//        test.enqueu(0);
+//        test.enqueu(4);
+//        test.enqueu(21);
+//
+//        test.dequeue();
